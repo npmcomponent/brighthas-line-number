cfa1e94c6b3b75459ae819c7074232e2719b1489
brighthas <brighthas@gmail.com>
2013-08-18 11:32:37 +0800

    publish version 0.0.1

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-line-number
-===========
 
-html pre code tag convert to have line number.
+# line-number
+
+  html's code tag convert to have line number table tag.
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install brighthas/line-number
+
+## API
+
+    var line = require("line-number");
+    var codeTag = document.querySelector("code");
+    var tableTag = line(codeTag);
+    
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,23 @@
+var dom = require("dom");
+module.exports = function parse(tag){
+
+    if(tag.tagName.toLowerCase() !== "code"){
+        tag = tag.querySelector("code")
+    }
+    
+    if(tag === null){
+        throw new Error("error tag ! sub tag contain code tag.");
+    }
+    
+    var code = tag.innerHTML;
+    lines = code.split(/\r?\n/).filter(function(str){
+        return !/^\s*$/gi.test(str)
+    });
+    var tableTagBodyStr = "";
+    lines.forEach(function(line,index){
+        tableTagBodyStr +='<tr>\
+        <td><td>'+(index+1)+'<td><pre><code>'+line+'</code></pre></td>\
+        </tr>'
+    })
+    return dom("<table>"+tableTagBodyStr+"</table>").els[0];
+}
